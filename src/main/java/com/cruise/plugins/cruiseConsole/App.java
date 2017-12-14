5d56e78b62aa2b860df424dd42380cd5058d6130
steve <stevechappell2000@yahoo.com>
2017-12-14 10:42:56 -0800

    init

---
+++ src/main/java/com/cruise/plugins/cruiseConsole/App.java
@@ -0,0 +1,13 @@
+package com.cruise.plugins.cruiseConsole;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
